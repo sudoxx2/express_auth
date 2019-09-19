5b3d26ca08eab271e727190d65a6be2a3661b07a
luv2c0de <moungpeter@gmail.com>
2019-09-18 19:18:56 -0700

    finished registration and hashed password func

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+
+const users = []
 
 app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { name: "kyle"})
@@ -13,6 +17,22 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('register.ejs', { name: "kyle"})
+})
+
+app.post('/register', async(req, res) => {
+  try{
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('register')
+  }
+  console.log(users)
 })
 
 app.listen(3000)
